81749ad2fba9759089a1f52f52a5be48bf1cdd0b
David Garcia Gonzalez <dgarci23@nd.edu>
2022-02-03 00:03:56 -0500

    Update gitignore and controller

--- hdl/controller.v
+++ hdl/controller.v
@@ -7,16 +7,21 @@ module controller
 	
 	(
 		input					CLOCK_50,
+		// UART signals to the DE2-115
 		output 					UART_TXD,
 		input					UART_RXD,
-		input			 [9:0]		SW,
-		input			 [1:0]		KEY,
-		output		 	 [10:0]		LEDR
+		// Read signal from the processor
+		input					RE,
+		input					WE
+		// UART data
+		input		[7:0]			send_data,
+		output		[7:0]			receive_data	
 	);
 	
-	reg state;	
 	wire [7:0] rx_data, tx_data;
-	wire rx_done, tx_busy;
+	wire rx_done, tx_busy, tx_done, rx_done, tx_empty, rx_empty;
+
+	assign tx_done = ~(tx_empty|tx_busy);
 	
 	rx_controller rx (
 		.clk(CLOCK_50),
@@ -27,63 +32,33 @@ module controller
 	
 	tx_controller tx (
 		.clk(CLOCK_50),
-		.TX_SEND(~(empty|tx_busy)&KEY_1),
+		.TX_SEND(tx_done),
 		.UART_TXD(UART_TXD),
 		.TX_DATA(tx_data),
 		.TX_DONE(),
 		.TX_BUSY(tx_busy)
 	);
-	
-	/*always @(posedge CLOCK_50)
-		state <= SW[8];*/
-		
-	wire empty;	
 		
 	FIFO fifo_tx (
 		.clk(CLOCK_50),
 		.rst(1'b0),
-		.in(SW[7:0]),
-		.we(TX_SEND_EDGE),
-		.re(~(empty|tx_busy)&KEY_1),
+		.in(send_data),
+		.we(WE),
+		.re(tx_done),
 		.out(tx_data),
-		.empty(empty),
-		.led(LEDR[8:0])
+		.empty(tx_empty),
 	);
 
 	FIFO fifo_rx (
 		.clk(CLOCK_50),
 		.rst(1'b0),
-		.in(),
-		.we(),
-		.re(),
-		.out(),
-		.empty()
+		.in(tx_data),
+		.we(rx_done),
+		.re(RE),
+		.out(receive_data),
+		.empty(rx_done)
 	);
 	
-	assign LEDR[10] = tx_busy;
-
-	assign LEDR[9] = empty;
-	
-	wire TX_SEND_DB, TX_SEND_EDGE;
-	
-	debouncer debouncer (
-		.clk(CLOCK_50),
-		.in(~KEY[0]),
-		.out(TX_SEND_DB)
-	);
-	
-	debouncer debouncer1 (
-		.clk(CLOCK_50),
-		.in(~KEY[1]),
-		.out(KEY_1)
-	);
-	
-	edge_detector edge_detector (
-		.clk(CLOCK_50),
-		.in(TX_SEND_DB),
-		.out(TX_SEND_EDGE)
-	);
-		
 endmodule
 					
 					
